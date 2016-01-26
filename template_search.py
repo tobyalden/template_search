b75b8fe89ce6b649a1a2baeb3dc0d9777f66584e
Toby Alden <talden@discogsinc.com>
2016-01-26 11:03:51 -0800

    Create list of extended template names

--- template_search.py
+++ template_search.py
@@ -1,11 +1,18 @@
+import re
 from sys import argv
 
 filename = argv[1]
 search = argv[2]
 template = open(filename)
 
+filenames = []
+
 print "Searching %r..." % filename
 for index, line in enumerate(template):
 	if search in line : print index, line,
+	if 'extends' in line : 
+		match = re.search('\"(.*?)\"|\'(.*?)\'', line)
+		filenames.append(match.group(match.lastindex))
 
+print(filenames)
 template.close()
